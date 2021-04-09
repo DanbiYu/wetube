1f39c4aa0694aaa703ce412405da5a43e385dd76
danbi <tryeui@allwinedu.com>
2021-04-09 14:44:49 +0900

    wip

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # WeTube
 
-loning Youtube with Vanilla and NodeJS
+Cloning Youtube with Vanilla and NodeJS
+
+
+## Pages:
+
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [x] Edit Profile
+- [x] Change Password
+- [x] Upload
+- [ ] Video Detail
+- [x] Edit Video
+  
+
+
+ 
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,36 @@
-export const join = (req, res) => res.render("join");
-export const login = (req, res) => res.render("login");
-export const logout = (req, res) => res.render("logout");
+import routes from "../routes";
+export const getJoin = (req, res) => {
 
+    res.render("join");
+}
+
+export const postJoin = (req, res) => {
+    const{
+        body: {name, email, password, password2}
+    } = req;
+    // 비밀번호와 비밀번호 확인 값이 다르면 회원가입 할 수 없도록 하기
+    if(password !== password2){
+        res.status(400);
+        res.render("join", { pageTitle: "Join" });
+    } else {
+        // To Do: Register User
+        // To DO: Log user in
+        res.redirect(routes.home)
+    }
+}
+
+export const getLogin = (req, res) => res.render("login", {pageTitle: "Log In"});
+export const postLogin = (req, res) => {
+    res.redirect(routes.home);
+}
+
+
+
+export const logout = (req, res) => {
+    // To Do: Process Log out
+    res.redirect(routes.home);
+}
+
+export const userDetail = (req, res) => res.render("userDetail");
+export const editProfile = (req, res) => res.render("editProfile");
+export const changePassword = (req, res) => res.render("changePassword");
